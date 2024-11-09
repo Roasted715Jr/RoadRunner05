86eab15a4fd772df5d8186799abb49af746b74d0
Matthew Duphily <matthew.duphily@live.com>
2024-11-08 22:50:45 -0500

    Disabled some opmodes and started pathing. Need to get a better idea of how RR generates splines

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRAuto.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -17,7 +18,7 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
  * exercise is to ascertain whether the localizer has been configured properly (note: the pure
  * encoder localizer heading may be significantly off if the track width has not been tuned).
  */
-@Autonomous(group = "drive")
+@Autonomous()
 @Config
 public class RRAuto extends LinearOpMode {
     public static double chainSpeed = 0.5;
@@ -25,8 +26,19 @@ public class RRAuto extends LinearOpMode {
 
     public static double startX = 32;
     public static double startY = 61.5;
-    public static double startHeading = 3 * Math.PI / 2;
+    public static double startHeadingDeg = 270;
+    double startHeading = Math.toRadians(startHeadingDeg); //Convet to radians so we don't need to constantly convert
     public static boolean observationSide = false;
+
+    public static Vector2d block1Apex = new Vector2d(36, 12);
+    public static double block1ApexTanDeg = 0;
+    double block1ApexTan= Math.toRadians(block1ApexTanDeg);
+    public static Vector2d block1Contact = new Vector2d(48, 24);
+    public static double block1ContactTanDeg = 90;
+    double block1ContactTan = Math.toRadians(block1ContactTanDeg);
+    public static Vector2d block1Finish = new Vector2d(48, 61.5);
+    public static double block1FinishTanDeg = 90;
+    double block1FinishTan = Math.toRadians(block1FinishTanDeg);
 
     public static double forwardDistance = 53;
     public static double leftDistance = 14.5;
@@ -34,9 +46,15 @@ public class RRAuto extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
+        //Initialize RoadRunner mecanum
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
-        Pose2d startPose = new Pose2d(startX, startY, startHeading);
+        drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
+        //Initialize RoadRunner position
+        Pose2d startPose = new Pose2d(startX, startY, startHeading);
+        drive.setPoseEstimate(startPose);
+
+        //Initialize other robot hardware
         DcMotor chain = hardwareMap.get(DcMotor.class, "chain");
         chain.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         DcMotor angle = hardwareMap.get(DcMotor.class, "angle");
@@ -44,27 +62,35 @@ public class RRAuto extends LinearOpMode {
         Servo leftServo = hardwareMap.get(Servo.class, "leftServo");
         Servo rightServo = hardwareMap.get(Servo.class, "rightServo");
 
-        drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
 //        Trajectory autoTrajectory = drive.trajectoryBuilder(startPose)
 //                .forward(forwardDistance)
 //                .strafeLeft(leftDistance)
 //                .back(backDistance)
 //                .build();
 
-        Trajectory forwardTraj = drive.trajectoryBuilder(startPose)
-                .forward(forwardDistance)
+//        Trajectory forwardTraj = drive.trajectoryBuilder(startPose)
+//                .forward(forwardDistance)
+//                .build();
+//
+//        Trajectory leftTraj = drive.trajectoryBuilder(forwardTraj.end())
+//                .strafeLeft(leftDistance)
+//                .build();
+//
+//        Trajectory backTraj = drive.trajectoryBuilder(leftTraj.end())
+//                .back(backDistance)
+//                .build();
+
+        //Roadrunner suggests that whenever the robot stops or reverses direction, we should create a new trajectory https://learnroadrunner.com/trajectories.html#running-multiple-trajectories
+        Trajectory block1Traj = drive.trajectoryBuilder(startPose)
+//                .splineToConstantHeading(block1Apex, block1ApexTan)
+                .lineToConstantHeading(block1Apex)
                 .build();
 
-        Trajectory leftTraj = drive.trajectoryBuilder(forwardTraj.end())
-                .strafeLeft(leftDistance)
+        Trajectory block1FinishTraj = drive.trajectoryBuilder(block1Traj.end(), true)
+                .splineToConstantHeading(block1Contact, block1ContactTan)
+//                .splineToConstantHeading(block1Finish, block1FinishTan)
+                .lineToConstantHeading(block1Finish)
                 .build();
-
-        Trajectory backTraj = drive.trajectoryBuilder(leftTraj.end())
-                .back(backDistance)
-                .build();
-
-        drive.setPoseEstimate(startPose);
 
         waitForStart();
 
@@ -72,13 +98,11 @@ public class RRAuto extends LinearOpMode {
             return;
 
 //        drive.followTrajectory(autoTrajectory);
-        drive.followTrajectory(forwardTraj);
-        drive.followTrajectory(leftTraj);
-        drive.followTrajectory(backTraj);
+//        drive.followTrajectory(forwardTraj);
+//        drive.followTrajectory(leftTraj);
+//        drive.followTrajectory(backTraj);
 
-        while (!isStopRequested()) {
-
-        }
-
+        drive.followTrajectory(block1Traj);
+        drive.followTrajectory(block1FinishTraj);
     }
 }
